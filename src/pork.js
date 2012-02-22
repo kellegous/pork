f63d40335eccc14f785d76a0eea9a966849cecaf
Kelly Norton <knorton@google.com>
2012-02-22 18:56:03 -0500

    Added xhrGet to pork.js

--- src/pork.js
+++ src/pork.js
@@ -3,6 +3,29 @@ var pork = {};
 #include "pork/grid.js"
 
 /**
+@param url {string}
+@param xhrDidSucceed {?function(XMLHttpRequest)}
+@param xhrDidFail {?function(XMLHttpRequest)}
+*/
+var xhrGet = function(url, xhrDidSucceed, xhrDidFail) {
+  var xhr = new XMLHttpRequest;
+  xhr.open('GET', url, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      if (xhr.status == 200) {
+        if (xhrDidSucceed)
+          xhrDidSucceed(xhr);
+      } else {
+        if (xhrDidFail)
+          xhrDidFail(xhr);
+      }
+      xhr = null;
+    }
+  };
+  xhr.send(null);
+};
+
+/**
  @param c {function()}
 */
 pork.whenReady = function(c) {
