285e4b52317ad42c6d1ab8e5a50be3587e172f86
Kelly Norton <kellegous@gmail.com>
2014-03-23 15:41:59 -0400

    Updating command line tool.

--- tools/pork/pork.go
+++ tools/pork/pork.go
@@ -1,32 +1,55 @@
 package main
 
 import (
-	"flag"
-	"github.com/kellegous/pork"
-	"log"
-	"net/http"
+  "flag"
+  "fmt"
+  "github.com/kellegous/pork"
+  "log"
+  "net/http"
+  "os"
 )
 
+func mainServe(args []string) {
+  flags := flag.NewFlagSet("serve", flag.ExitOnError)
+  flagAddr := flags.String("addr", ":8082", "address to bind")
+  flags.Parse(args)
+
+  var dirs []http.Dir
+  if flags.NArg() == 0 {
+    dirs = append(dirs, http.Dir("."))
+  } else {
+    for _, arg := range args {
+      dirs = append(dirs, http.Dir(arg))
+    }
+  }
+
+  r := pork.NewRouter(func(status int, r *http.Request) {
+    log.Printf("[%d] %s", status, r.RequestURI)
+  }, nil, nil)
+
+  r.RespondWith("/", pork.Content(pork.NewConfig(pork.None), dirs...))
+
+  if err := http.ListenAndServe(*flagAddr, r); err != nil {
+    log.Panic(err)
+  }
+}
+
+func mainBuild(args []string) {
+  fmt.Println("TODO(knorton): Implement build.")
+}
+
 func main() {
-	flagAddr := flag.String("addr", ":8082", "address to bind")
+  if len(os.Args) <= 1 {
+    fmt.Println("help!")
+    os.Exit(1)
+  }
 
-	flag.Parse()
-
-	var dirs []http.Dir
-	if flag.NArg() == 0 {
-		dirs = append(dirs, http.Dir("."))
-	} else {
-		for _, arg := range flag.Args() {
-			dirs = append(dirs, http.Dir(arg))
-		}
-	}
-
-	r := pork.NewRouter(func(status int, r *http.Request) {
-		log.Printf("[%d] %s", status, r.RequestURI)
-	}, nil, nil)
-	r.RespondWith("/", pork.Content(pork.NewConfig(pork.None), dirs...))
-
-	if err := http.ListenAndServe(*flagAddr, r); err != nil {
-		log.Panic(err)
-	}
+  switch os.Args[1] {
+  case "serve":
+    mainServe(os.Args[1:])
+  case "build":
+    mainBuild(os.Args[1:])
+  default:
+    fmt.Fprintln(os.Stderr, "Invalid command")
+  }
 }
